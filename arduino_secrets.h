e2ea083c3ba7f46576f40596b277c5bec9677619
Adam Youngblood <ayoungblood84@gmail.com>
2018-11-19 17:11:38 -0500

    added secret file

---
+++ arduino_secrets.h
@@ -0,0 +1,10 @@
+#define SECRET_AWSKEY "YOUR_AWS_SECRET_KEY";
+#define SECRET_AWSSECRET "YOUR_AWS_SECRET";
+#define SECRET_AWSREGION "YOUR_AWS_REGION";
+#define SECRET_AWSEP "YOUR AWS MQTT ENDPOINT";
+
+#define SECRET_WIFISSID "YOUR WIFI SSID (WIFI NAME)";
+#define SECRET_WIFIPW "YOUR WIFI PASSWORD";
+
+#define SECRET_OTAPASSWORD "YourPassword" // change this to whatever password you want to use when you upload OTA, or dont.
+#define SECRET_VERSION "1.0" // I use this to track the versions of code across my many IoT devices (It will show in the JSON of the MQTT message.)
